d1b227c1a245e7e986e3e52956aa6f883438f815
田村和也 <tamurakazuya@tamurakazuyanoMacBook-Pro.local>
2025-12-26 10:32:55 +0100

    finished day2

--- day2/main.py
+++ day2/main.py
@@ -15,18 +15,18 @@ def get_first_and_last(input: str) -> tuple[int, int]:
     return int(first_str), int(last_str)
 
 
-def is_valid(num: int) -> bool:
-    str_num = str(num)
-    if len(str_num) % 2 != 0:
-        return False
-    half = len(str_num) // 2
-    return str_num[:half] == str_num[half:]
-
-
 def part_one():
-    sample_input = get_part_1_input(PACKAGE_PATH, ParseType.comma)
+    input = get_part_1_input(PACKAGE_PATH, ParseType.comma)
     total = 0
-    for line in sample_input:
+
+    def is_valid(num: int) -> bool:
+        str_num = str(num)
+        if len(str_num) % 2 != 0:
+            return False
+        half = len(str_num) // 2
+        return str_num[:half] == str_num[half:]
+
+    for line in input:
         first, last = get_first_and_last(line)
         for num in range(first, last + 1):
             if is_valid(num):
@@ -35,13 +35,32 @@ def part_one():
 
 
 def part_two():
-    sample_input = get_sample_2_input(PACKAGE_PATH, ParseType.comma)
-    for line in sample_input:
-        print(get_first_and_last(line))
+    input = get_part_2_input(PACKAGE_PATH, ParseType.comma)
+    total = 0
+
+    def is_valid(num: int) -> bool:
+        str_num = str(num)
+        for i in range(len(str_num) - 1):
+            if len(str_num) % (i + 1) != 0:
+                continue
+            separated = [
+                str_num[j : j + (i + 1)] for j in range(0, len(str_num), (i + 1))
+            ]
+            if all(part == separated[0] for part in separated):
+                return True
+        return False
+
+    for line in input:
+        first, last = get_first_and_last(line)
+        for num in range(first, last + 1):
+            if is_valid(num):
+                total += num
+
+    return total
 
 
 if __name__ == "__main__":
     print("Part One:")
     print(part_one())
-    # print("Part Two:")
-    # part_two()
+    print("Part Two:")
+    print(part_two())
