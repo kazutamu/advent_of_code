b7aeb40b05877521f2fa16ac31a8d3f64becbf04
田村和也 <tamurakazuya@tamurakazuyanoMacBook-Pro.local>
2025-12-26 17:10:55 +0100

    extended the input utils

---
+++ day5/main.py
@@ -0,0 +1,27 @@
+from utils.input_utils import (
+    ParseType,
+    get_sample_1_input,
+    get_part_2_input,
+    get_part_1_input,
+    get_sample_2_input,
+)
+from pathlib import Path
+
+PACKAGE_PATH = Path(__file__).parent
+
+
+def part_one():
+    inputs = get_sample_1_input(PACKAGE_PATH, ParseType.two_section_lines)
+    print(inputs)
+
+
+def part_two():
+    inputs = get_sample_2_input(PACKAGE_PATH, ParseType.two_section_lines)
+    print(inputs)
+
+
+if __name__ == "__main__":
+    print("Part One:")
+    part_one()
+    print("Part Two:")
+    part_two()
--- utils/input_utils.py
+++ utils/input_utils.py
@@ -6,6 +6,7 @@ class ParseType(StrEnum):
     comma = "comma"
     lines = "lines"
     matrix = "matrix"
+    two_section_lines = "two_section_lines"
 
 
 def get_list_from_line_file(file_path: Path) -> list[str]:
@@ -25,6 +26,17 @@ def get_matrix_from_file(file_path: Path) -> list[list[str]]:
         return [list(line) for line in lines]
 
 
+def get_two_section_lines_from_file(file_path: Path) -> tuple[list[str], list[str]]:
+    with open(file_path, "r") as file:
+        content = file.read().strip()
+        sections = content.split("\n\n")
+        if len(sections) != 2:
+            raise ValueError("Input file does not contain exactly two sections.")
+        section1 = sections[0].splitlines()
+        section2 = sections[1].splitlines()
+        return section1, section2
+
+
 def parse_input(file_path: Path, parse_type: ParseType) -> list[str]:
     if parse_type == ParseType.lines:
         return get_list_from_line_file(file_path)
@@ -32,6 +44,8 @@ def parse_input(file_path: Path, parse_type: ParseType) -> list[str]:
         return get_list_from_comma_file(file_path)
     elif parse_type == ParseType.matrix:
         return get_matrix_from_file(file_path)
+    elif parse_type == ParseType.two_section_lines:
+        return get_two_section_lines_from_file(file_path)
     else:
         raise ValueError(f"Unknown parse type: {parse_type}")
 
